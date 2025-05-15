755fd12fc61dbeb4d630dbc767e14200c74031ae
p00persnitzel <93892509+p00persnitzel@users.noreply.github.com>
2025-05-14 17:07:01 -0700

    Fix image loading issues by using URL-friendly filenames

---
+++ rename_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Rename image files to URL-friendly names
+mv "SIX_5E92850E-9F53-4057-BBFC-C8AA8149FCE7 (1).PNG" "anime-comparison-1.png"
+mv "SIX_2B43A493-5CF8-49B8-B213-1883F3831486 (1).PNG" "anime-comparison-2.png"
+mv "SIX_924F382D-7A8E-4E63-8B57-4F75DA702169.PNG" "anime-comparison-3.png"
+mv "SIX_1FB35B1E-F9B5-4F2F-9DCF-CE223044BCFE.PNG" "anime-comparison-4.png"
+mv "VbsAGp5U_400x400.jpg" "believe-logo.jpg" 
